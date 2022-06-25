58d11474d1345f8aa1a2ddfa1a5d42c23f4d9c8e
jkintner25 <jkintner25@gmail.com>
2022-06-24 23:39:34 -0500

    finished notebooks store for now

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import notebooksReducer from "./notebooks";
 import sessionReducer from './session';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
-
+  notebooks: notebooksReducer,
 });
 
 let enhancer;
---
+++ frontend/src/store/notebooks.js
@@ -0,0 +1,88 @@
+import { csrfFetch } from "./csrf";
+
+//types
+const ADD_NOTEBOOK = "notebooks/ADD_NOTEBOOK";
+const GET_NOTEBOOKS = "notebooks/GET_NOTEBOOKS";
+const UPDATE_NOTEBOOK = "notebooks/UPDATE_NOTEBOOK";
+const REMOVE_NOTEBOOK = "notebooks/REMOVE_NOTEBOOK";
+
+//actions
+const add = (notebook) => ({
+    type: ADD_NOTEBOOK,
+    notebook
+});
+
+const load = (notebooks) => ({
+    type: GET_NOTEBOOKS,
+    notebooks
+});
+
+const edit = (notebook) => ({
+    type: UPDATE_NOTEBOOK,
+    notebook
+});
+
+const remove = (notebook) => ({
+    type: REMOVE_NOTEBOOK,
+    notebook
+});
+
+//thunk middleware
+export const addOneNotebook = (payload) => async dispatch => {
+    const response = await csrfFetch(`/api/notebooks`, {
+        method: 'POST',
+        body: JSON.stringify(payload)
+    });
+    const notebook = await response.json();
+    dispatch(add(notebook));
+};
+
+export const getAllNotebooks = (userId) => async dispatch => {
+    const response = await csrfFetch(`/api/notebooks/${userId}`,);
+    if (response.ok) {
+        const notebooks = await response.json();
+        dispatch(load(notebooks));
+    };
+};
+
+export const editNotebook = (id, payload) => async dispatch => {
+    const response = await csrfFetch(`/api/edit/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(payload)
+    });
+    const notebook = await response.json();
+    dispatch(edit(notebook));
+};
+
+export const deleteNotebook = (id) => async dispatch => {
+    const response = await csrfFetch(`/api/delete/${id}`, {
+        method: 'DELETE',
+    });
+    const notebook = await response.json();
+    dispatch(remove(notebook));
+};
+
+const initialState = { notes: null }
+
+const notebooksReducer = (state = initialState, action) => {
+    const newState = {}
+    switch (action.type) {
+        case ADD_NOTEBOOK:
+            return { ...state, [action.notebook.id]: action.notebook };
+        case GET_NOTEBOOKS:
+            newState = action.notebooks.forEach(notebook => {
+                newState[notebook.id] = notebook;
+            });
+            return newState;
+        case UPDATE_NOTEBOOK:
+            return { ...state, [action.notebook.id]: action.notebook };
+        case REMOVE_NOTEBOOK:
+            newState = { ...state };
+            delete newState[action.notebook.id];
+            return newState;
+        default:
+            return state;
+    };
+};
+
+export default notebooksReducer;
